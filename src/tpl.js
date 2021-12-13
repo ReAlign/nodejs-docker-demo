8e94de3838d3e49c23fd86d49fb7332c1eda9166
ReAlign <realign@yeah.net>
2021-12-13 23:49:06 +0800

    re:update for private register

--- src/tpl.js
+++ src/tpl.js
@@ -3,15 +3,17 @@ const preStyle = 'style="background: #eee; border-radius: 4px; padding: 20px;"';
 
 module.exports = (data = {}) =>
   `
-  <h2>${data.title}</h2>
-  <b ${quoteStyle}>|</b>&nbsp;&nbsp;
-  <i ${quoteStyle}>${data.statusText} - ${data.date}</i>
-  <p>The data is:</p>
-  ${
-    data.result
-      ? `<code>
-          <pre ${preStyle}>${JSON.stringify(data.result, null, 4)}</pre>
-        </code>`
-      : 'None'
-  }
+  <div style="width: 800px; margin: 0 auto;">
+    <h2>${data.title}</h2>
+    <b ${quoteStyle}>|</b>&nbsp;
+    <i ${quoteStyle}>${data.statusText} - ${data.date}</i>
+    <p>The data is:</p>
+    ${
+      data.result
+        ? `<code>
+            <pre ${preStyle}>${JSON.stringify(data.result, null, 4)}</pre>
+          </code>`
+        : 'None'
+    }
+  </div>
 `;
