07c9919ee0d95f5a3c26b8a6c7dfcb023f0021d2
ReAlign <realign@yeah.net>
2021-12-16 13:53:00 +0800

    re:mongo change

--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
 'use strict';
 
 const express = require('express');
+const images = require("images");
+
 const getData = require('./mongodb');
 const tpl = require('./tpl');
 
 // Constants
 const PORT = 8085;
-const HOST = '0.0.0.0';
+const HOST = '127.0.0.1';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   const title = 'Node.js Mongodb Demo';
   const date = new Date();
+  // res.send(tpl({
+  //   title,
+  //   statusText: 'successful',
+  //   date,
+  //   result: [{"status": "None"}],
+  // }));
   getData()
     .then((result) => {
       res.send(tpl({
@@ -32,6 +40,11 @@ app.get('/', (req, res) => {
         },
       }));
     });
+});
+
+app.get('/img', (req, res) => {
+  res.setHeader('content-type', 'image/png')
+  res.send( images(200, 100).fill(255, 0, 0, 1).encode("png") );
 });
 
 app.listen(PORT, HOST);
