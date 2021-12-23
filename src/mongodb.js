dbc36f3e245a254710c2e82230b257da4892cbeb
ReAlign <realign@yeah.net>
2021-12-24 03:10:01 +0800

    re:172.22.0.8

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://realign-mongo_extnetwork:27017';
+const url = 'mongodb://172.22.0.8:27017';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
