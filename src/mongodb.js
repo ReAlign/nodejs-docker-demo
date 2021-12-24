baa214da7591f37fe6811b80e73e16111c8d7ef7
ReAlign <realign@yeah.net>
2021-12-24 22:25:37 +0800

    re:mongolink

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://mongo:27017';
+const url = 'mongodb://172.22.0.8:27017';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
